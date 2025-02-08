91db15aa9820c0b3a458bf6f1fa65df428fb47c2
ANDONIAINA Fenosoa Sarobidy <fenosoasarobidy42@gmail.com>
2025-02-08 07:07:50 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,18 @@
+//ce programme affirme à l'utilisateur si le produit deux nombres est positif ou negatif
+#include <stdio.h>
+int main()
+{
+    float n,p;
+    printf ("Veuillez entrer un nombre n: ");
+    scanf("%f", &n);
+    printf ("Veuillez entrer un nombre p: ");
+    scanf("%f", &p);
+    if (p>0 && n>0 || p<0 && n<0)
+    {
+        printf ("Leur produit est positif");
+    }
+    else
+    {
+        printf("Leur produit est negatif");
+    }
+}
