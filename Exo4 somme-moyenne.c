eea21b6d715a367b25ed8d74dbfc75b70045c93c
ANDONIAINA Fenosoa Sarobidy <fenosoasarobidy42@gmail.com>
2025-02-08 07:26:44 +0300

    Update Exo4 somme-moyenne.c

--- Exo4 somme-moyenne.c
+++ Exo4 somme-moyenne.c
@@ -3,12 +3,12 @@
 int main()
 {
     float S,n,p,M;
-    printf ("Entrer un nombre n=");
+    printf("Entrer un nombre n=");
     scanf("%f", &n);
-    printf ("Entrer un nombre p=");
+    printf("Entrer un nombre p=");
     scanf("%f", &p);
     S=n+p;
     M=S/2;
-    printf ("Leur somme est S= %f\nLeur moyenne est M=%f", S,M);
+    printf("Leur somme est S= %f\nLeur moyenne est M=%f", S,M);
     return (0);
 }
