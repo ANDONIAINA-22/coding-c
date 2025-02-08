abbaae9d4f8610a68fa954d4f5e59b545be0db79
ANDONIAINA Fenosoa Sarobidy <fenosoasarobidy42@gmail.com>
2025-02-08 07:00:18 +0300

    Add files via upload

---
+++ main.c
@@ -0,0 +1,17 @@
+//Ce programme informe si un nombre est positif ou negatif
+#include <stdio.h>
+int main()
+{
+    int n;
+    printf("Entrer un nombre n: ");
+    scanf("%d", &n);
+    if (n>=0)
+    {
+        printf("n est positif");
+    }
+        else
+        {
+            printf ("n est negatif");
+        }
+    return(0);
+}
